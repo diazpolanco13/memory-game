af54b7e4577a1063d9744673e2787f47c5140b27
Carlos Diaz <diazpolanco13@gmail.com>
2020-08-01 19:38:29 -0400

    no entiendo

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "./styles/Carta.css";
+import FlipCard from "react-flipcard";
 
 export default class Carta extends Component {
   render() {
     return (
       <div className="carta">
-        <i className={`fa ${this.props.icono} fa-5x`}></i>
+        <FlipCard>
+          <div className="portada"></div>
+          <div className="contenido">
+            <i className={`fa ${this.props.icono} fa-5x`}></i>
+          </div>
+        </FlipCard>
       </div>
     );
   }
