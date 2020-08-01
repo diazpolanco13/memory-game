e40bc4d7e5090d322cb53722e3aa73710a183118
Carlos Diaz <diazpolanco13@gmail.com>
2020-08-01 18:35:47 -0400

    insertando barajas al tablero

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header.jsx";
 import Tablero from "./Tablero.jsx";
 import "./styles/App.css";
+import construirBaraja from "./utils/construirBaraja.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Tablero />
-    </div>
-  );
+// Metodo que retorna el estado de la app
+const getEstadoInicial = () => {
+  const baraja = construirBaraja();
+  return {
+    baraja,
+  };
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getEstadoInicial();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Tablero baraja={this.state.baraja} />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/Tablero.jsx
+++ src/components/Tablero.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import Carta from "./Cartas.jsx";
+import Carta from "./Carta.jsx";
 import "./styles/Tablero.css";
 
 export default class Tablero extends Component {
   render() {
-    const cartas = [1, 2, 3, 4, 5];
     return (
       <div className="tablero">
-        {cartas.map((carta) => (
-          <Carta></Carta>
+        {this.props.baraja.map((carta) => (
+          <Carta icono={carta.icono} />
         ))}
       </div>
     );
